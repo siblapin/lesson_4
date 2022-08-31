bc4fc43ca1f7621b91a498a6aebbdc5e667d834a
siblapin <sib.lapin@mail.ru>
2022-08-31 23:31:33 +0700

    Lesson_4

---
+++ 04_StatefulStatelessWidget/list_view_keys_scroll/lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<String> nav = ['Список 1', 'Список 2'];
+  final List fakeData = List.generate(100, (index) => index.toString());
+
+  final data = [
+    Image.network('https://picsum.photos/1200/501'),
+    Image.network('https://picsum.photos/1200/502'),
+    Image.network('https://picsum.photos/1200/503'),
+    Image.network('https://picsum.photos/1200/504'),
+    Image.network('https://picsum.photos/1200/505'),
+    Image.network('https://picsum.photos/1200/506'),
+    Image.network('https://picsum.photos/1200/507'),
+    Image.network('https://picsum.photos/1200/508'),
+    Image.network('https://picsum.photos/1200/509'),
+    Image.network('https://picsum.photos/1200/510'),
+  ];
+  final data1 = [
+    Image.network('https://picsum.photos/1200/501'),
+    Image.network('https://picsum.photos/1200/502'),
+    Image.network('https://picsum.photos/1200/503'),
+    Image.network('https://picsum.photos/1200/504'),
+    Image.network('https://picsum.photos/1200/505'),
+    Image.network('https://picsum.photos/1200/506'),
+    Image.network('https://picsum.photos/1200/507'),
+    Image.network('https://picsum.photos/1200/508'),
+    Image.network('https://picsum.photos/1200/509'),
+    Image.network('https://picsum.photos/1200/510'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: nav
+                .map((String item) => Tab(
+                      text: item,
+                    ))
+                .toList(),
+          ),
+          title: Text('Homework example'),
+        ),
+        body: TabBarView(children: [
+          Container(
+            child: data1.first,
+          ),
+          Text('e')
+        ]),
+      ),
+    );
+  }
+}
